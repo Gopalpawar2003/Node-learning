3a04c8c267c21528ff7b9bbbcba77d2b173144e4
Gopalpawar <kp015770gmail.com>
2026-03-13 19:00:16 +0530

    first commit

---
+++ conditional.js
@@ -0,0 +1,6 @@
+const age = 22;
+if (age >= 18) {
+    console.log('can vote');
+} else {
+    console.log('cannot vote');
+}
---
+++ variables.js
@@ -0,0 +1,5 @@
+let name = 'Gopal';
+let city = 'Delhi';
+let age = 22;
+console.log('Name: ' + name + ', City: ' + city + ', Age: ' + age);
+console.log(`name : ${name}, City: ${city}, age : ${age}`);
