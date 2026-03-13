344f806315d70ef1343600ed43f8340279efe8db
Gopalpawar <kp015770gmail.com>
2026-03-13 19:21:28 +0530

    arguments file added and read file learing in node

---
+++ argument.js
@@ -0,0 +1,8 @@
+let num1 = Number(process.argv[2]) || 0;
+let num2 = Number(process.argv[3]) || 0;
+
+let add = num1 + num2;
+
+// console.log(`The sum is: ${add}`);
+
+console.log(process.argv.length);
---
+++ greet.js
@@ -0,0 +1,2 @@
+let name = Processing.argv[2] || 'friend';
+console.log(`Hello, ${name}!`);
